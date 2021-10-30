1931abe076e3ed78f939c82ddc4b03488cc605cb
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-30 19:30:37 +0200

    a element created

---
+++ GET Request/profil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>You can see all profiles which you want to see here!!</h3>
+
+<span class="name">
+    <?php
+    if(isset($_GET["id"]))
+    {
+        if()
+    }
+     
+
+
+
+</span>
+
+
+</body>
+</html>
