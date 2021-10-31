c2ff409e46957527edab1378e5b7f8544200b3e2
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-31 19:15:14 +0100

    add and substract  made

--- Calculator/index.php
+++ Calculator/index.php
@@ -38,13 +38,26 @@ $first_num = $_POST['first_number'];
 $second_num = $_POST['second_number'];
 $operator = $_POST['operator'];
 $result = '';
+
+if(is_numeric($first_num) && is_numeric($second_num))
+{
+    switch ($operator)
+    {
+        case "Add":
+            $result = $first_num + $second_num;
+            break;
+        case "Substract":
+            $result = $first_num - $second_num;
+            break;
+    }
+}
 ?>
 <body>
     <h4>BASIC CALCULATOR WITH PHP</h4>
     <form action="" method="post" id="calculator">
-        <input type="number" name="first_number" placeholder="Please enter first number" id="first_number"> <br>
-        <input type="number" name="second_number" placeholder="Please enter second number" id="second_number"> <br>
-        <input type="readonly" name="result" placeholder="Result">
+        <input type="number" name="first_number" placeholder="Please enter first number" id="first_number" value="<?php echo $first_num; ?>" required> <br>
+        <input type="number" name="second_number" placeholder="Please enter second number" id="second_number" value="<?php echo $second_num; ?>" required> <br>
+        <input type="readonly" name="result" placeholder="Result" value="<?php echo $result ?>">
 
         <div class="box">
         <input type="submit" name="operator" value="Add"> <br>
