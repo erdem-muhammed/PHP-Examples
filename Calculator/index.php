7306ad58b9b554ab489c73955d3a2805dd6962c4
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-31 18:42:57 +0100

    form created

---
+++ Calculator/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form input
+        {
+            width: 200px;
+            padding: 15px;
+            border-radius: 8px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h4>BASIC CALCULATOR WITH PHP</h4>
+    <form action="" method="post" id="calculator">
+        <input type="number" name="first_number" placeholder="Please enter first number"> <br>
+        <input type="number" name="second_number" placeholder="Please enter second number"> <br>
+        <input type="readonly" name="result" placeholder="Result">
+    </form>
+</body>
+</html>
