b290e28ea33505c48824ca04a824d165443983f1
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-01 23:27:26 +0100

    index style updated

--- Login-Php/index.php
+++ Login-Php/index.php
@@ -5,6 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body
+        {
+            height: 100vh;
+            background-color: #f7f6f2;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
     <form method="post" action="http://localhost/Login-Php/login.php">
