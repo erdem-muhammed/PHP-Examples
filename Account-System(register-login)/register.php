f3ea247e856b3c5a8d378efd23a2800714777b17
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-04 13:14:20 +0100

    register html added

--- Account-System(register-login)/register.php
+++ Account-System(register-login)/register.php
@@ -17,3 +17,20 @@ empty($_POST["email"]) || empty($_POST["pass"]) || empty($_POST["pass2"]))
     }
 
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    You have successfully registered!!
+
+</body>
+</html>
