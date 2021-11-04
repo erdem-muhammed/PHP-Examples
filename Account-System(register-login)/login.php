fe8b2d45e72a827718b5fca48e62b82371ac4042
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-04 13:02:45 +0100

    form created

---
+++ Account-System(register-login)/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Log in</h1>
+    <form method="POST" action="loggedin.php">
+        <input type="email" placeholder="Email" name="email" required>
+        <input type="password" placeholder="Password" name="pass" required>
+        <input type="submit" value="Loggedin">
+    </form>
+    
+</body>
+</html>
