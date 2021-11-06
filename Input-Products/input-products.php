0f3fa14fd63102ad9823a46ed008d63947cc4b52
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-06 19:14:00 +0100

    form created

---
+++ Input-Products/input-products.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" placeholder="Title" name="title">
+        <textarea placeholder="Features" name="features"></textarea>
+        <input type="text" placeholder="Price" name="price">
+        <input type="text" placeholder="Stock" name="stock">
+        <input type="submit" value="add">
+    </form>
+</body>
+</html>
